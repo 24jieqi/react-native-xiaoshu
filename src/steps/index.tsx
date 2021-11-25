837fcf92fe5af00aa34b82a250098c71a6039e74
winljm001 <819648983@qq.com>
2021-11-25 18:38:39 +0800

    feat: auto scroll

--- src/steps/index.tsx
+++ src/steps/index.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, memo } from 'react'
+import React, { createContext, memo, useRef } from 'react'
 import type { FC } from 'react'
 import { ScrollView, View } from 'react-native'
 import { useTheme, widthStyle } from '../theme'
@@ -9,13 +9,19 @@ export const StepsContext = createContext<{
   current?: number
   data?: StepsPropsType['data']
 }>({})
+const maxSteps = 3
 const Steps: FC<StepsPropsType> = ({ current, data, style }) => {
   const THEME_VAR = useTheme()
+  const scrollRef = useRef<ScrollView>(null)
   const STYLES = widthStyle(THEME_VAR, createStyles)
   let inner = null
   if (data?.length > 0) {
     inner = (
-      <View style={[STYLES.stepsBox, style]}>
+      <View
+        style={[STYLES.stepsBox, style]}
+        onLayout={e => {
+          setScrollDistance(e?.nativeEvent?.layout?.width)
+        }}>
         <StepsContext.Provider value={{ current, data }}>
           {data?.map((v, i) => {
             return <Step key={i} index={i} {...v} />
@@ -24,10 +30,21 @@ const Steps: FC<StepsPropsType> = ({ current, data, style }) => {
       </View>
     )
   }
+  const setScrollDistance = width => {
+    if (scrollRef.current && data?.length > maxSteps) {
+      scrollRef.current.scrollTo({
+        x: (width / data?.length) * (current - 1 > 0 ? current - 1 : 0),
+        y: 0,
+      })
+    }
+  }
   return (
     <View style={STYLES.outWrap}>
-      {data?.length > 3 ? (
-        <ScrollView style={STYLES.scrollViewBox} horizontal>
+      {data?.length > maxSteps ? (
+        <ScrollView
+          style={STYLES.scrollViewBox}
+          horizontal
+          ref={ref => (scrollRef.current = ref)}>
           {inner}
         </ScrollView>
       ) : (
