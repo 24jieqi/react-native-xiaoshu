a70b580befe644a1e907b89e8c9a70ad10601fe6
LING_ZI_QING <onlylingstudio@gmail.com>
2022-03-17 17:25:20 +0800

    feat(Theme): 🎸 调整 tokens 变量类型

--- src/theme/interface.ts
+++ src/theme/interface.ts
@@ -1,14 +1,14 @@
 import type TOKENS from './tokens-mix'
 
 /** tokens */
-export type TokensType = typeof TOKENS & {
-  [key in string]: any
-}
+export type TokensType = typeof TOKENS
 
 /** 外界可以参与修改的变量 */
 export type StyleVar = Partial<TokensType>
 
 /** 默认初始化状态 */
 export interface ThemeProviderProps {
-  theme?: StyleVar
+  theme?: StyleVar & {
+    [key in string]: any
+  }
 }
