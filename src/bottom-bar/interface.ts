44accc49c66658c62cbd7fea9401fe5f8d90c8de
LING_ZI_QING <onlylingstudio@gmail.com>
2022-04-01 19:32:32 +0800

    docs(BottomBar): ✏️ 注释规范

--- src/bottom-bar/interface.ts
+++ src/bottom-bar/interface.ts
@@ -27,7 +27,7 @@ export interface BottomBarProps extends ViewProps {
 
   /**
    * 当软键盘弹出来的时候不渲染，BottomBar 固定在底部的布局中，在 Android 机器上 BottomBar 会被软键盘挤上去
-   * @description true
+   * @default true
    */
   keyboardShowNotRender?: boolean
 }
