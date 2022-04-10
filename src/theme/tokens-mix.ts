31802380b7ce11253ef03aa834cfcefe722c2b1c
LING_ZI_QING <onlylingstudio@gmail.com>
2022-04-10 16:14:15 +0800

    refactor(Theme): 💡 变量添加注释

--- src/theme/tokens-mix.ts
+++ src/theme/tokens-mix.ts
@@ -2,8 +2,25 @@ import TOKENS from '@fruits-chain/design-tokens-bailu'
 
 export default {
   ...TOKENS,
+  /**
+   * 动画过渡时间
+   * @default 300
+   */
   animation_duration_base: 300,
+  /**
+   * 动画过渡时间
+   * @default 200
+   */
   animation_duration_fast: 200,
+  /**
+   * 通用边框颜色 gray_4
+   * @default #E3E5E8
+   */
   border_color: TOKENS.gray_4,
+
+  /**
+   * 点击反馈背景颜色
+   * @default 'transparent'
+   */
   underlay_color: 'transparent',
 }
