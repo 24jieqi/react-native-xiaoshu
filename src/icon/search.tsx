295cb8ec46d8ddb9731abc759ca5bf2ed865c55d
LING_ZI_QING <onlylingstudio@gmail.com>
2022-04-13 10:37:59 +0800

    feat(Icon): 🎸 更新图标

--- src/icon/search.tsx
+++ src/icon/search.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { Path, G } from 'react-native-svg'
+import { Path } from 'react-native-svg'
 
 import { genIcon } from './helper/gen'
 
 const IconSearchOutline = genIcon({
   render: color => {
     return (
-      <G fill={color} fillRule="nonzero">
-        <Path d="M6.822 6.33a8.556 8.556 0 1 1 12.1 12.1 8.556 8.556 0 0 1-12.1-12.1zm1.06 1.06a7.056 7.056 0 1 0 9.979 9.979A7.056 7.056 0 0 0 7.883 7.39z" />
-        <Path d="M18.391 17.899a.75.75 0 0 1 .984-.067l.077.067 3.038 3.038a.75.75 0 0 1-.984 1.128l-.076-.067-3.039-3.038a.75.75 0 0 1 0-1.061z" />
-      </G>
+      <Path
+        fill={color}
+        fillRule="nonzero"
+        d="M5.226 5.231A8.556 8.556 0 0 1 17.83 16.78l.025.021 3.038 3.039a.75.75 0 0 1-.984 1.128l-.077-.067-3.038-3.039-.021-.024A8.556 8.556 0 0 1 5.226 5.23zm1.06 1.061a7.056 7.056 0 1 0 9.979 9.978 7.056 7.056 0 0 0-9.979-9.978z"
+      />
     )
   },
 })
