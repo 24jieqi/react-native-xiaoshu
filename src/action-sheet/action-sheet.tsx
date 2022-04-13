d0ec947fee1162f6146085bd5836173f51c680b1
LING_ZI_QING <onlylingstudio@gmail.com>
2022-04-13 20:16:45 +0800

    refactor(ActionSheet): 💡 优化弹出层最大高度

--- src/action-sheet/action-sheet.tsx
+++ src/action-sheet/action-sheet.tsx
@@ -2,11 +2,9 @@ import React, { memo, isValidElement } from 'react'
 import { Text, View, ScrollView } from 'react-native'
 
 import Button from '../button'
-import { varCreator as varCreatorButton } from '../button/style'
 import Divider from '../divider'
 import { isDef } from '../helpers'
 import { useSafeHeight } from '../hooks'
-import { varCreator as varCreatorNavBar } from '../nav-bar/style'
 import Popup from '../popup/popup'
 import PopupHeader from '../popup/popup-header'
 import Theme from '../theme'
@@ -32,8 +30,6 @@ const ActionSheet: React.FC<ActionSheetProps> = ({
   const safeHeight = useSafeHeight({ top: safeAreaInsetTop })
   const TOKENS = Theme.useThemeTokens()
   const CV = Theme.createVar(TOKENS, varCreator)
-  const CV_NAV_BAR = Theme.createVar(TOKENS, varCreatorNavBar)
-  const CV_BUTTON = Theme.createVar(TOKENS, varCreatorButton)
   const STYLES = Theme.createStyle(CV, styleCreator)
   const isTitleDef = isDef(title)
   const isCancelTextDef = isDef(cancelText)
@@ -58,55 +54,48 @@ const ActionSheet: React.FC<ActionSheetProps> = ({
     )
   ) : null
 
-  const btnHeight = CV_BUTTON.button_xl_height
-  const descriptionHeight =
-    CV.action_sheet_description_line_height + (isTitleDef ? 12 : 12 * 2) + 1
-  const contentHeight =
-    safeHeight -
-    (isTitleDef ? CV_NAV_BAR.nav_bar_height : 0) -
-    (isCancelTextDef ? CV.action_sheet_cancel_padding_top + btnHeight : 0) -
-    (isDescriptionDef ? descriptionHeight : 0)
-
   return (
     <Popup {...restProps} safeAreaInsetBottom position="bottom" round={round}>
-      {isTitleDef ? <PopupHeader title={title} showClose={false} /> : null}
-      {descriptionJSX}
+      <View style={{ maxHeight: safeHeight }}>
+        {isTitleDef ? <PopupHeader title={title} showClose={false} /> : null}
+        {descriptionJSX}
 
-      <ScrollView style={{ maxHeight: contentHeight }} bounces={false}>
-        {actions.map((item, index) => {
-          return (
+        <ScrollView bounces={false}>
+          {actions.map((item, index) => {
+            return (
+              <Button
+                key={`${item.name}_${index}`}
+                text={item.name}
+                disabled={item.disabled}
+                loading={item.loading}
+                color={item.color || CV.action_sheet_text_color}
+                type="link"
+                size="xl"
+                textStyle={STYLES.button_text}
+                onPress={() => {
+                  if (!item.disabled && !item.loading) {
+                    onSelect?.(item, index)
+                  }
+                }}
+              />
+            )
+          })}
+        </ScrollView>
+
+        {isCancelTextDef ? (
+          <>
+            <View style={STYLES.gap} />
             <Button
-              key={`${item.name}_${index}`}
-              text={item.name}
-              disabled={item.disabled}
-              loading={item.loading}
-              color={item.color || CV.action_sheet_text_color}
+              text={cancelText}
               type="link"
               size="xl"
+              color={CV.action_sheet_text_color}
               textStyle={STYLES.button_text}
-              onPress={() => {
-                if (!item.disabled && !item.loading) {
-                  onSelect?.(item, index)
-                }
-              }}
+              onPress={onCancel}
             />
-          )
-        })}
-      </ScrollView>
-
-      {isCancelTextDef ? (
-        <>
-          <View style={STYLES.gap} />
-          <Button
-            text={cancelText}
-            type="link"
-            size="xl"
-            color={CV.action_sheet_text_color}
-            textStyle={STYLES.button_text}
-            onPress={onCancel}
-          />
-        </>
-      ) : null}
+          </>
+        ) : null}
+      </View>
     </Popup>
   )
 }
