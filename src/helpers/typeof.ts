37b08978bffa2f8e54484824a811d191efb407cc
LING_ZI_QING <onlylingstudio@gmail.com>
2022-04-15 16:38:32 +0800

    refactor(ci): 💡 移除没有用到的函数

--- src/helpers/typeof.ts
+++ src/helpers/typeof.ts
@@ -22,16 +22,3 @@ export const isObject = <T>(v: T): v is T => isType('Object')(v)
 export const isPromise = <T = any>(val: any): val is Promise<T> => {
   return isObject(val) && isFunction(val.then) && isFunction(val.catch)
 }
-
-/** 是手机号码 */
-export function isMobile(value: string): boolean {
-  value = value.replace(/[^-|\d]/g, '')
-  return (
-    /^((\+86)|(86))?(1)\d{10}$/.test(value) || /^0[0-9-]{10,13}$/.test(value)
-  )
-}
-
-/** 是空 */
-export function isNullish(value: any) {
-  return ['', undefined, null].includes(value)
-}
