98b7ae78f17a5c259077537d1d3e9dc06d229ca1
LING_ZI_QING <onlylingstudio@gmail.com>
2022-05-09 16:28:19 +0800

    test(Button): 💍 更新测试

--- src/button/__tests__/button.test.tsx
+++ src/button/__tests__/button.test.tsx
@@ -1,9 +1,11 @@
 import TOKENS from '@fruits-chain/design-tokens-bailu'
 import React from 'react'
+import type { ViewStyle } from 'react-native'
+import type { ReactTestInstance } from 'react-test-renderer'
 import { create } from 'react-test-renderer'
 
 import Button from '..'
-import { customRender } from '../../__tests__/test-utils'
+import { customRender, fireEvent } from '../../__tests__/test-utils'
 
 describe('Button', () => {
   it('render snapshot', () => {
@@ -14,28 +16,90 @@ describe('Button', () => {
     expect(tree).toMatchSnapshot()
   })
 
-  it('color id brand_6', () => {
+  it('color', () => {
     const { getByA11yLabel } = customRender(
-      <Button accessibilityLabel="button" type="primary" text="primary" />,
+      <>
+        <Button accessibilityLabel="button-primary" text="primary" />
+        <Button accessibilityLabel="button-#000" text="primary" color="#000" />
+        <Button accessibilityLabel="button-ghost" type="ghost" text="primary" />
+      </>,
     )
 
-    expect(getByA11yLabel('button').parent.props.style.backgroundColor).toEqual(
-      TOKENS.brand_6,
-    )
+    const buttonPrimary = getByA11yLabel('button-primary')
+    const buttonPrimaryStyle: ViewStyle = buttonPrimary.props.style
+    const buttonPrimaryText = buttonPrimary.children[0] as ReactTestInstance
+
+    expect(buttonPrimaryStyle.backgroundColor).toEqual(TOKENS.brand_6)
+    expect(buttonPrimaryStyle.borderColor).toEqual(TOKENS.brand_6)
+    expect(buttonPrimaryText.props.style.color).toEqual(TOKENS.white)
+
+    const button000 = getByA11yLabel('button-#000')
+    const button000Style: ViewStyle = button000.props.style
+    const button000Text = buttonPrimary.children[0] as ReactTestInstance
+
+    expect(button000Style.backgroundColor).toEqual('#000')
+    expect(button000Style.borderColor).toEqual('#000')
+    expect(button000Text.props.style.color).toEqual(TOKENS.white)
+
+    const buttonGhost = getByA11yLabel('button-ghost')
+    const buttonGhostStyle: ViewStyle = buttonGhost.props.style
+    const buttonGhostText = buttonGhost.children[0] as ReactTestInstance
+
+    expect(buttonGhostStyle.backgroundColor).toEqual('transparent')
+    expect(buttonGhostStyle.borderColor).toEqual(TOKENS.brand_6)
+    expect(buttonGhostText.props.style.color).toEqual(TOKENS.brand_6)
   })
 
-  it('color id "#098"', () => {
+  it('onPress', () => {
+    const buttonOnPress = jest.fn()
     const { getByA11yLabel } = customRender(
       <Button
-        accessibilityLabel="button"
-        type="primary"
-        color="#098"
+        accessibilityLabel="button-primary"
         text="primary"
+        onPress={buttonOnPress}
       />,
     )
 
-    expect(getByA11yLabel('button').parent.props.style.backgroundColor).toEqual(
-      '#098',
+    const buttonPrimary = getByA11yLabel('button-primary')
+
+    fireEvent(buttonPrimary.children[0] as ReactTestInstance, 'press')
+
+    expect(buttonOnPress).toHaveBeenCalled()
+  })
+
+  it('onPress:disabled', () => {
+    const buttonOnPress = jest.fn()
+    const { getByA11yLabel } = customRender(
+      <Button
+        accessibilityLabel="button-primary"
+        text="primary"
+        onPress={buttonOnPress}
+        disabled
+      />,
     )
+
+    const buttonPrimary = getByA11yLabel('button-primary')
+
+    fireEvent(buttonPrimary.children[0] as ReactTestInstance, 'press')
+
+    expect(buttonOnPress).not.toHaveBeenCalled()
+  })
+
+  it('onPress:loading', () => {
+    const buttonOnPress = jest.fn()
+    const { getByA11yLabel } = customRender(
+      <Button
+        accessibilityLabel="button-primary"
+        text="primary"
+        onPress={buttonOnPress}
+        loading
+      />,
+    )
+
+    const buttonPrimary = getByA11yLabel('button-primary')
+
+    fireEvent(buttonPrimary.children[0] as ReactTestInstance, 'press')
+
+    expect(buttonOnPress).not.toHaveBeenCalled()
   })
 })
