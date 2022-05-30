48da6935e06eda01ce6a64003b5055c00a291720
LING_ZI_QING <onlylingstudio@gmail.com>
2022-05-30 14:55:40 +0800

    feat(Selector): 🎸 禁止选项新增文字颜色区分

--- src/selector/style.ts
+++ src/selector/style.ts
@@ -8,6 +8,7 @@ export const varCreator = (TOKENS: TokensType) => {
     selector_option_text_line_height: 50,
     selector_option_text_font_size: TOKENS.font_size_4,
     selector_option_text_color: TOKENS.gray_8,
+    selector_option_text_disabled_color: TOKENS.gray_6,
     selector_icon_selected_color: TOKENS.brand_6,
     selector_body_padding_horizontal: TOKENS.space_3,
   }
@@ -31,6 +32,10 @@ export const styleCreator = (cv: ComponentVars) => {
       fontSize: cv.selector_option_text_font_size,
       color: cv.selector_option_text_color,
       // backgroundColor: '#f30', // to test ui
+    },
+
+    option_item_text_disabled: {
+      color: cv.selector_option_text_disabled_color,
     },
 
     btn: {
