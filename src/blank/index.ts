8ee1ea17a74370f85cf6f9098e4d342a5f3517fc
LING_ZI_QING <onlylingstudio@gmail.com>
2022-06-03 14:02:53 +0800

    refactor(Blank): 💡 新增导出变量\样式函数

---
+++ src/blank/index.ts
@@ -0,0 +1,6 @@
+import { attachPropertiesToComponent } from '../helpers'
+
+import Blank from './blank'
+import { varCreator } from './style'
+
+export default attachPropertiesToComponent(Blank, { varCreator })
