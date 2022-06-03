0f800be0e6c64c26be63ed4d0c6ca034ed5e453d
LING_ZI_QING <onlylingstudio@gmail.com>
2022-06-03 14:40:57 +0800

    refactor(Checkbox): 💡 新增导出变量\样式函数

--- src/checkbox/index.ts
+++ src/checkbox/index.ts
@@ -2,7 +2,10 @@ import { attachPropertiesToComponent } from '../helpers'
 
 import Checkbox from './checkbox'
 import CheckboxIcon from './checkbox-icon'
+import { varCreator, styleCreator } from './style'
 
 export default attachPropertiesToComponent(Checkbox, {
+  varCreator,
+  styleCreator,
   Icon: CheckboxIcon,
 })
