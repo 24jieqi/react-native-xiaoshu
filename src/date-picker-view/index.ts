8073b05eec84d6a4aa615a0ef4d6320a4428f4af
LING_ZI_QING <onlylingstudio@gmail.com>
2022-06-03 14:59:40 +0800

    refactor(DatePickerView): 💡 导出内部用的 hook

--- src/date-picker-view/index.ts
+++ src/date-picker-view/index.ts
@@ -2,7 +2,11 @@ import { attachPropertiesToComponent } from '../helpers'
 
 import DatePickerView from './date-picker-view'
 import { formatDate } from './helper'
+import useDateMinMax from './useDateMinMax'
+import useDatePicker from './useDatePicker'
 
 export default attachPropertiesToComponent(DatePickerView, {
   formatDate,
+  useDateMinMax,
+  useDatePicker,
 })
