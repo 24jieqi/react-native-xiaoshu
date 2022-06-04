96155656b8046f895163aafa56d1c81d553ff0c7
LING_ZI_QING <onlylingstudio@gmail.com>
2022-06-04 15:40:45 +0800

    refactor(Progress): 💡 新增导出变量\样式函数

--- src/progress/index.ts
+++ src/progress/index.ts
@@ -2,7 +2,9 @@ import { attachPropertiesToComponent } from '../helpers'
 
 import Progress from './progress'
 import ProgressPage from './progress-page'
+import { varCreator } from './style'
 
 export default attachPropertiesToComponent(Progress, {
+  varCreator,
   Page: ProgressPage,
 })
