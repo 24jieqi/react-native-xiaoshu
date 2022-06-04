f3fa7205de37c02bb95466d45dc67133988c42c3
LING_ZI_QING <onlylingstudio@gmail.com>
2022-06-04 15:34:31 +0800

    refactor(Popover): 💡 新增导出变量\样式函数

--- src/popover/index.ts
+++ src/popover/index.ts
@@ -3,8 +3,11 @@ import { attachPropertiesToComponent } from '../helpers'
 import Popover from './popover'
 import PopoverItem from './popover-item'
 import PopoverText from './popover-text'
+import { varCreator, styleCreator } from './style'
 
 export default attachPropertiesToComponent(Popover, {
+  varCreator,
+  styleCreator,
   Item: PopoverItem,
   Text: PopoverText,
 })
