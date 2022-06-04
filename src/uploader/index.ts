da1c66cc22b1e4f3bc8685b643cecd00cf2457c7
LING_ZI_QING <onlylingstudio@gmail.com>
2022-06-04 16:51:32 +0800

    refactor(Uploader): 💡 新增导出变量\样式函数

--- src/uploader/index.ts
+++ src/uploader/index.ts
@@ -1,8 +1,11 @@
 import { attachPropertiesToComponent } from '../helpers'
 
+import { varCreator, styleCreator } from './style'
 import Uploader from './uploader'
 import UploaderRegular from './uploader-regular'
 
 export default attachPropertiesToComponent(Uploader, {
+  varCreator,
+  styleCreator,
   Regular: UploaderRegular,
 })
