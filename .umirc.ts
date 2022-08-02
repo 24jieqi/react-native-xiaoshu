82cee89acd691b6fdbed2216b0bf3f5a129afdfa
winljm001 <819648983@qq.com>
2022-08-02 10:40:46 +0800

    docs: 去掉打包

--- .umirc.ts
+++ .umirc.ts
@@ -1,19 +1,44 @@
 import type WebpackChain from 'webpack-chain'
 import { defineConfig } from 'dumi'
 import p from './package.json'
-const FileManagerPlugin = require('filemanager-webpack-plugin')
 const pkgJSON = require('./package.json')
 const CopyPlugin = require('copy-webpack-plugin')
+const navs = [
+  {
+    title: '指南',
+    path: '/guide',
+  },
+  {
+    title: '组件',
+    path: '/component',
+  },
+  {
+    title: '案例',
+    path: '/case',
+  },
+  {
+    title: `GitHub v${p.version}`,
+    path: 'https://github.com/hjfruit/react-native-xiaoshu',
+  },
+  {
+    title: '更新日志',
+    path: 'https://github.com/hjfruit/react-native-xiaoshu/releases',
+  },
+  {
+    title: '下载 Android 预览 APK',
+    path: `https://www.onlyling.com/apks/${p.name}-v${p.version}.apk`,
+  },
+]
 // 生成版本json文件
 const CopyWebpackPlugin = new CopyPlugin({
   patterns: [
     {
       from: './package.json',
-      to: './app-zip-info.json',
+      to: './app-info.json',
       transform: () => {
         return JSON.stringify(
           {
-            url: `https://hjfruit.github.io/xiaoshu-doc/${pkgJSON.name}-v${pkgJSON.version}.zip`,
+            navs,
             appName: pkgJSON.name,
           },
           null,
@@ -23,32 +48,12 @@ const CopyWebpackPlugin = new CopyPlugin({
     },
   ],
 })
-// 把生成的静态资源打包成zip
-const fileManagerPlugin = new FileManagerPlugin({
-  events: {
-    onEnd: {
-      archive: [
-        {
-          source: './docs-dist',
-          destination: `./zips/${pkgJSON.name}-v${pkgJSON.version}.zip`,
-        },
-      ],
-      move: [
-        {
-          source: `./zips/${pkgJSON.name}-v${pkgJSON.version}.zip`,
-          destination: `./docs-dist/${pkgJSON.name}-v${pkgJSON.version}.zip`,
-        },
-      ],
-    },
-  },
-})
 const repo = process.env.PUBLIC_PATH || ''
 
 export default defineConfig({
   chainWebpack(memo: WebpackChain) {
     memo.plugins.delete('copy')
     if (process.env.NODE_ENV !== 'development') {
-      memo.plugin('file-manager-plugin').use(fileManagerPlugin)
       memo.plugin('copy-webpack-plugin').use(CopyWebpackPlugin)
     }
   },
@@ -63,7 +68,6 @@ export default defineConfig({
   logo: 'https://avatars.githubusercontent.com/u/74942048',
   base: `/${repo}`,
   publicPath: `./${repo}`,
-  runtimePublicPath: true,
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   apiParser: {
     propFilter: {
@@ -71,32 +75,7 @@ export default defineConfig({
       skipNodeModules: true,
     },
   },
-  navs: [
-    {
-      title: '指南',
-      path: '/guide',
-    },
-    {
-      title: '组件',
-      path: '/component',
-    },
-    {
-      title: '案例',
-      path: '/case',
-    },
-    {
-      title: `GitHub v${p.version}`,
-      path: 'https://github.com/hjfruit/react-native-xiaoshu',
-    },
-    {
-      title: '更新日志',
-      path: 'https://github.com/hjfruit/react-native-xiaoshu/releases',
-    },
-    {
-      title: '下载 Android 预览 APK',
-      path: `https://www.onlyling.com/apks/${p.name}-v${p.version}.apk`,
-    },
-  ],
+  navs,
   resolve: {
     includes: ['docs', 'src'],
   },
