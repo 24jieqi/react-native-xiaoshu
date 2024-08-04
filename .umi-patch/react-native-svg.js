fa68a3fe135398fe17d025da1f309df653038a4a
LING_ZI_QING <onlylingstudio@gmail.com>
2024-08-04 11:45:43 +0800

    docs(docs): ✏️ 修复 SvgXml

--- .umi-patch/react-native-svg.js
+++ .umi-patch/react-native-svg.js
@@ -1,3 +1,22 @@
 export * from 'react-native-svg/src/ReactNativeSVG.web'
 
-export const SvgXml = () => {}
+const SvgXmlStyle = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  alignSelf: 'center',
+}
+
+/**
+ * @param {{xml: string}} props
+ */
+export const SvgXml = props => {
+  return (
+    <div
+      style={SvgXmlStyle}
+      dangerouslySetInnerHTML={{
+        __html: props.xml,
+      }}
+    />
+  )
+}
