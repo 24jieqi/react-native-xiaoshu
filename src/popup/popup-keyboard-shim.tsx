f3257557a3793875304a52be8db45141b68c36bb
LING_ZI_QING <onlylingstudio@gmail.com>
2025-06-22 10:46:12 +0800

    feat(Popup): 🎸 优化 PopupKeyboardShim

--- src/popup/popup-keyboard-shim.tsx
+++ src/popup/popup-keyboard-shim.tsx
@@ -4,28 +4,37 @@ import type { KeyboardEvent, KeyboardEventName, View } from 'react-native'
 
 import type { PopupKeyboardShimProps } from './interface'
 
-const listenerEventType : {  show: KeyboardEventName ; hide: KeyboardEventName} = Platform.select({
-  ios: {
-    show: 'keyboardWillShow',
-    hide: 'keyboardWillHide'
-  },
-  android: {
-    show: 'keyboardDidShow',
-    hide: 'keyboardDidHide'
-  },
-  default: {
-    show: 'keyboardWillShow',
-    hide: 'keyboardWillHide'
-  }
-})
+const listenerEventType: { show: KeyboardEventName; hide: KeyboardEventName } =
+  Platform.select({
+    ios: {
+      show: 'keyboardWillShow',
+      hide: 'keyboardWillHide',
+    },
+    android: {
+      show: 'keyboardDidShow',
+      hide: 'keyboardDidHide',
+    },
+    default: {
+      show: 'keyboardWillShow',
+      hide: 'keyboardWillHide',
+    },
+  })
 
-const PopupKeyboardShim: React.FC<PopupKeyboardShimProps> = props => {
+const PopupKeyboardShim: React.FC<PopupKeyboardShimProps> = ({
+  allowOnAndroid = false,
+  ...props
+}) => {
   const KeyboardHeight = useRef(new Animated.Value(0))
   const ViewHeight = useRef<View>(null)
   const { height } = useWindowDimensions()
 
   useEffect(() => {
-    if (Platform.OS === 'ios' || Platform.OS === 'android') {
+    if (
+      // iOS 软键盘出来覆盖内容区域
+      Platform.OS === 'ios' ||
+      // Android 根据自身应用配置不同，软键盘可能挤压内容区域，也可能覆盖内容区域
+      (allowOnAndroid && Platform.OS === 'android')
+    ) {
       const keyboardDidShow = (e: KeyboardEvent) => {
         // eslint-disable-next-line max-params
         ViewHeight.current?.measure(
@@ -75,7 +84,7 @@ const PopupKeyboardShim: React.FC<PopupKeyboardShimProps> = props => {
         }
       }
     }
-  }, [height])
+  }, [height, allowOnAndroid])
 
   return (
     <Animated.View
